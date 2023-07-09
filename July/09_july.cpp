790a181164cf87723dabf886b07cc3e63a3173b1
Ravvviii <91066307+ravvviii@users.noreply.github.com>
2023-07-09 07:28:45 +0530

    Add files via upload

---
+++ July/09_july.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    int missingNumber(int arr[], int n) 
+    { 
+        // Your code here
+           for (int i = 0; i < n; i++)
+    {
+        if (arr[i] <= 0 || arr[i] > n)
+            arr[i] = n + 1;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int index = abs(arr[i]);
+
+        if (index <= n)
+        {
+            if (arr[index - 1] > 0)
+                arr[index - 1] = -arr[index - 1];
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > 0)
+            return i + 1;
+    }
+
+    return n + 1;
+    } 
+};
+
+//{ Driver Code Starts.
+
+int missingNumber(int arr[], int n);
+
+int main() { 
+    
+    //taking testcases
+    int t;
+    cin>>t;
+    while(t--){
+        
+        //input number n
+        int n;
+        cin>>n;
+        int arr[n];
+        
+        //adding elements to the array
+        for(int i=0; i<n; i++)cin>>arr[i];
+        
+        Solution ob;
+        //calling missingNumber()
+        cout<<ob.missingNumber(arr, n)<<endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
