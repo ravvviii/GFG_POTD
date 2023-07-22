7eded832b673ed1207ef886e0e68d175607b2de9
Ravvviii <91066307+ravvviii@users.noreply.github.com>
2023-07-22 07:14:36 +0530

    Add files via upload

---
+++ July/22_july.cpp
@@ -0,0 +1,110 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+void print(Node *root)
+{
+Node *temp = root;
+while(temp!=NULL)
+{
+cout<<temp->data<<" ";
+temp=temp->next;
+}
+}
+
+
+// } Driver Code Ends
+/*
+The structure of linked list is the following
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+
+class Solution
+{
+    public:
+    //Function to remove duplicates from unsorted linked list.
+    Node * removeDuplicates( Node *head) 
+    {
+     // your code goes here
+     if (!head)
+            return head;
+
+        unordered_set<int> seen_values;
+        Node *current = head;
+        seen_values.insert(current->data);
+
+        while (current->next)
+        {
+            if (seen_values.find(current->next->data) != seen_values.end())
+            {
+                // Skip the duplicate node
+                Node *temp = current->next;
+                current->next = current->next->next;
+                delete temp;
+            }
+            else
+            {
+                // Move to the next node and add its value to the set
+                current = current->next;
+                seen_values.insert(current->data);
+            }
+        }
+
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+ 
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+		
+	    Solution ob;
+		Node *result  = ob.removeDuplicates(head);
+		print(result);
+		cout<<endl;
+		
+	}
+	return 0;
+}
+// } Driver Code Ends
